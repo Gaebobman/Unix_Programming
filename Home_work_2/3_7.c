4659c6c8d1f0b153a7feda340012b0fbd7641081
Lee <jplee04@naver.com>
2021-10-03 19:24:20 +0900

    10.03

--- Home_work_2/3_7.c
+++ Home_work_2/3_7.c
@@ -5,25 +5,36 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <math.h>
+#include <string.h>
 
 #define PERM_LENGTH 9
 #define OCT_PERM_LENGTH 4
 
-const int WEIGHT[] = {4, 2, 1, 4, 2, 1, 4, 2, 1};
+const char WEIGHT[] = {'4', '2', '1', '4', '2', '1', '4', '2', '1'};
 
-void lsoct(char *permission,  int *oct_permission);
+mode_t lsoct(char *permission);
 
 int main(int argc, char *argv[])
 {
     int oct_permission[OCT_PERM_LENGTH] = {0};
-    int integer_oct_permission = 0;
-   
-   printf("%d\n", atoi(argv[2]));
+    mode_t mode;
     if (access(argv[1], F_OK) == 0)
     {
-        if (sizeof(argv[2]) == 8)
+        if (argv[2][0] == '0')
         { // if user gave octal permission argument
-            if (chmod(argv[1], atoi(argv[2])) != -1)
+            // Change argv to octal value
+
+            char S = argv[2][0] - '0';
+            char USER_CHAR = argv[2][1] - '0';
+            char GROUP_CHAR = argv[2][2] - '0';
+            char OTHER_CHAR = argv[2][3] - '0';
+
+            mode = OTHER_CHAR;
+            mode |= GROUP_CHAR << 3;
+            mode |= USER_CHAR << 6;
+            mode |= S << 9;
+
+            if (chmod(argv[1], mode) != -1)
             {
                 printf("Permissions have changed: %s\n", argv[2]);
             }
@@ -34,29 +45,28 @@ int main(int argc, char *argv[])
         }
         else
         { // if user gave ls permission argument
-            for(int i = 0 ; i < 2; i++){
-                integer_oct_permission += oct_permission[i] * pow(10.0, (double)(2-i));
-            }
-            if (chmod(argv[1], integer_oct_permission) != -1)
+            mode = lsoct(argv[2]);
+            if (chmod(argv[1], mode) != -1)
             {
-                printf("Permission changed: %d\n", integer_oct_permission);
-
+                printf("Permission changed: %s\n", argv[2]);
             }
             else
             {
                 perror("Permissions have not changed: ");
             }
+            
         }
     }
     else
     {
-        printf("File (%s)does not exists", argv[1]);
+        printf("File (%s)does not exists\n", argv[1]);
     }
     return 0;
 }
 
-void lsoct(char *permission, int *oct_permission)
+mode_t lsoct(char *permission)
 {
+    char oct_permission[OCT_PERM_LENGTH] = {'0'};
     for (int i = 0; i <= PERM_LENGTH; i++)
     {
         if (permission[i] != '-')
@@ -66,26 +76,33 @@ void lsoct(char *permission, int *oct_permission)
             case 0:
             case 1:
             case 2:
-                oct_permission[1] += WEIGHT[i];
+                oct_permission[1] += WEIGHT[i] -'0';
                 break;
             case 3:
             case 4:
             case 5:
-                oct_permission[2] += WEIGHT[i];
+                oct_permission[2] += WEIGHT[i]-'0';
                 break;
             case 6:
             case 7:
             case 8:
-                oct_permission[3] += WEIGHT[i];
+                oct_permission[3] += WEIGHT[i]-'0';
                 break;
             default:
                 break;
             }
         }
     }
-    for (int i = 0; i < OCT_PERM_LENGTH; i++)
-    {
-        printf("%d", oct_permission[i]);
-    }
+    mode_t mode;
+    char S = oct_permission[3];
+    char USER_CHAR = oct_permission[2];
+    char GROUP_CHAR = oct_permission[1];
+    char OTHER_CHAR = oct_permission[0];
 
+    mode = OTHER_CHAR;
+    mode |= GROUP_CHAR << 3;
+    mode |= USER_CHAR << 6;
+    mode |= S << 9;
+
+    return mode;
 }
